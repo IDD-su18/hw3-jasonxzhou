15e7268bbd648feff130b4315324ca688dd31722
Jason Zhou <jasonzhou@berkeley.edu>
2018-06-27 22:51:05 -0700

    software

--- hw3/hw3.ino
+++ hw3/hw3.ino
@@ -1,31 +1,40 @@
-//distance sensor => note selection 
+//distance sensor => note selection
+//pressure sensor => note velocity 
 //joystick axis 1 => pitch bend
-//joystick axis 2 => unassigned (return to midpoint)
-//potentiometer => unassigned 
+//joystick axis 2 => wavetable position (mod wheel mapped)
+//potentiometer => cutoff (midi learn)
 
 void setup() {
   Serial.begin(31250);
 }
 
 void loop() {
-for (int note = 0x1E; note < 0x5A; note ++) {
-    //Note on channel 1 (0x90), some note value (note), middle velocity (0x45):
-    //pitch bend noteOn(0xE0, 0, 50);
-    noteOn(0x90, note, 0x45);
-    delay(100);
-  
-    //Note on channel 1 (0x90), some note value (note), silent velocity (0x00):
-    noteOn(0x90, note, 0x00);
-    delay(100);
+
+}
+
+//Sends MIDI signal
+void sendMIDI(int firstByte, int secondByte, int thirdByte) {
+  Serial.write(firstByte);
+  Serial.write(secondByte);
+  Serial.write(thirdByte);
+}
+
+/* notes    
+    //notes range 0x1E to 0x5A
+    //pitch bend
+    //test
+    noteOn(0xE0, 0x00, 50);
+
+    //mod wheel
+    noteOn(0xB0, 0x01, i);
     
-  }
+    //Note on channel 1 (0x90), some note value (note), middle velocity (0x45):
+    noteOn(0x90, note, 0x00);
+    
+    //Note on channel 1 (0x90), some note value (note), silent velocity (0x00):
+    noteOn(0x90, note, 0x45);
 
-}
+    //midi learn to set any parameter might have to manually do this upon launching serum everytime though
+    sendMIDI(0xB0, 0x10, 1);
+  }*/
 
-// plays a MIDI note. Doesn't check to see that cmd is greater than 127, or that
-// data values are less than 127:
-void noteOn(int cmd, int pitch, int velocity) {
-  Serial.write(cmd);
-  Serial.write(pitch);
-  Serial.write(velocity);
-}
