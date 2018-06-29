112b85a3ed4d28213968f2420da0d1bcf91c9b46
Jason <jasonzhou@berkeley.edu>
2018-06-28 21:44:45 -0700

    html code

--- hw3/hw3.ino
+++ hw3/hw3.ino
@@ -4,13 +4,49 @@
 //joystick axis 2 => wavetable position (mod wheel mapped; value range: 1 to 1023)
 //potentiometer => cutoff (midi learn; value range: 1 to 1023)
 
+#include "Adafruit_VL53L0X.h"
+
+Adafruit_VL53L0X lox = Adafruit_VL53L0X();
+
 void setup() {
   //Setting baud rate
-  Serial.begin(31250);
+  Serial.begin(115200);
+  while (! Serial) {
+    delay(1);
+  }
+  
+  Serial.println("Adafruit VL53L0X test");
+  if (!lox.begin()) {
+    Serial.println(F("Failed to boot VL53L0X"));
+    while(1);
+  }
+  Serial.println(F("VL53L0X API Simple Ranging example\n\n")); 
+  
+  pinMode(A2, INPUT);
+  pinMode(A3, INPUT);
+  pinMode(A4, INPUT);
+  
+  pinMode(A5, INPUT);
+  pinMode(10, INPUT);
 }
 
 void loop() {
+  VL53L0X_RangingMeasurementData_t measure;
+    
+  Serial.print("Reading a measurement... ");
+  lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
 
+  if (measure.RangeStatus != 4) {  // phase failures have incorrect data
+    Serial.print("Distance (mm): "); Serial.println(measure.RangeMilliMeter);
+  } else {
+    Serial.println(" out of range ");
+  }
+    
+  delay(100);
+
+
+  
+  
 }
 
 //Sends MIDI signal
